568649052edea883ddba7cd99df6f907688aff7e
fccdelcampo <145944562+fccdelcampo@users.noreply.github.com>
2023-09-29 08:42:51 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -2,5 +2,11 @@ name = input("What is your name? ")
 age = int(input("What is your age? "))
 age_next_year = age + 1
 
+mod = 5 % 3
+pi = 3.14159
+
 print(f"Hello, {name}" + "!")
 print(f"My age next year is: {age_next_year}")
+
+printf("The remainder in 5 % 3 is", mod)
+printf("The approximate value of pi is", pi)
